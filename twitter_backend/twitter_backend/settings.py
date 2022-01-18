30cf371af0fb4ad321c2cb040c8cb61bdc9794bd
asales <augustcezar@gmail.com>
2022-01-17 23:51:05 -0300

    ajustes para deploy

--- twitter_backend/twitter_backend/settings.py
+++ twitter_backend/twitter_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import os
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
