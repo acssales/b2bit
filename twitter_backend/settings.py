020281e1939cb82e3e0a2f5ce781efb6cba2eeb0
asales <augustcezar@gmail.com>
2022-01-18 01:51:18 -0300

    Correcao no host

--- twitter_backend/settings.py
+++ twitter_backend/settings.py
@@ -26,9 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-django_heroku.settings(locals())
-
-ALLOWED_HOSTS = ["https://b2bit.herokuapp.com/api/user/", "127.0.0.1"]
+ALLOWED_HOSTS = ["https://b2bit.herokuapp.com/", "127.0.0.1"]
 
 # Application definition
 
